c2df7d93fc757303e485461be8c65ed3a89f500a
Chris Fogelklou <chris.fogelklou@gmail.com>
2025-04-12 09:14:17 +0200

    Fix vite.config.js so the app assets are found at applicaudia.se (#3)
    * Change name to locolibs
    * Devel is pushed automagically
    * updated vite.config so it can find the assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/locolibs/', // Set the base path for hosting in a subdirectory
   plugins: [react()],
 })
