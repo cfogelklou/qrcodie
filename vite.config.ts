7c1fe912e0ba5f8b7e06e1903b44bfc10cf054fe
Chris Fogelklou <chris.fogelklou@gmail.com>
2025-04-19 12:20:05 +0200

    Try changing base to '.' from '/measuredmunch'

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/measuredmunch/', // Set the base path for hosting in a subdirectory
+  base: '.', // Set the base path for hosting in a subdirectory
   plugins: [
     react(),
     VitePWA({
