0dcee5a394e56c2834e350c802f0fb0d8c813559
Chris Fogelklou <chris.fogelklou@gmail.com>
2025-04-22 07:35:20 +0200

    Fix icons

--- scripts/convert-svg-to-png.sh
+++ scripts/convert-svg-to-png.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This script converts SVG files to PNG format
-rsvg-convert -w 512 -h 512 ./icons/icon.svg -o ./icons/icon-512x512.png
-rsvg-convert -w 192 -h 192 ./icons/icon.svg -o ./icons/icon-192x192.png
+rsvg-convert -w 512 -h 512 ./public/icon.svg -o ./public/icon-512x512.png
+rsvg-convert -w 192 -h 192 ./public/icon.svg -o ./public/icon-192x192.png
